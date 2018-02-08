5d8dcae82322a61dff670d3f6d7a402bb5221595
rpersons1 <rpersons1@mail.csuchico.edu>
2018-02-08 12:31:56 -0800

    added <<one

--- main.cpp
+++ main.cpp
@@ -12,17 +12,17 @@ char one;
   //when user's entry is between A-Z...
   if(one>='A' && one<='Z')
   {
-  cout<<" is an upper case letter!\n";
+  cout<<one<<" is an upper case letter!\n";
   }
   else if(one>='a' && one <='z')
   {
   //when user's entry is between a-z...  
-  cout<<" is a lower case letter!\n";
+  cout<<one<<" is a lower case letter!\n";
   }
   else
   {
   //in all other cases...
-  cout<<"$! Pssh. What are you talking about?\n";
+  cout<<one<<"$?! Pssh. What are you talking about?\n";
   }
   //no matter what they enter...
   cout<<"The ASCII value is: "<<(int) one<<endl;
