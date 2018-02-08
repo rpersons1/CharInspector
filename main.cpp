08a2ce99a264f6c2a18e6405054b77e13c47e1a7
rpersons1 <rpersons1@mail.csuchico.edu>
2018-02-08 12:09:19 -0800

    made a char analysis program

--- main.cpp
+++ main.cpp
@@ -5,20 +5,28 @@ using namespace std;
 
 int main()
 {
-
+char one; 
+  
   cout<<"What character do you want to know about?\n";
-
+  cin>>one;
   //when user's entry is between A-Z...
+  if(one>='A' && one<='Z')
+  {
   cout<<" is an upper case letter!\n";
-
+  }
+  else if(one>='a' && one <='z')
+  {
   //when user's entry is between a-z...
+  
   cout<<" is a lower case letter!\n";
-
+  }
+  else
+  {
   //in all other cases...
   cout<<"?! Pssh. What are you talking about?\n";
-  
+  }
   //no matter what they enter...
-  cout<<"The ASCII value is: ";
+  cout<<"The ASCII value is: "<<(int) one<<endl;
 
   return 0;
 }
